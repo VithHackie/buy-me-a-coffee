18044c0f3e6b8c75f2113660790f27d88184792c
Krish Verma <129778286+VithHackie@users.noreply.github.com>
2026-06-25 23:25:42 +0530

    Update server.js

--- primary_server/server.js
+++ primary_server/server.js
@@ -21,13 +21,8 @@ app.post("/validate", async (req, res) => {
       to: body.email,
       subject: `Thank you for the coffee! ☕`,
       html: `<div style='font-family: Arial, sans-serif; color: #333; line-height: 1.6;'> \
-            <p><strong>Hello Friend,</strong></p> \
-            <p>I just wanted to reach out and send a sincere thank you for your generous donation of ${body.eth}eth. Your support means a lot to me and truly helps keep my work going!</p> \
-            <p>Since you are already familiar with my work, I wanted to see if you might be interested in collaborating on a project together. I am always looking to team up with great people, and I’d love to hear if you have any upcoming projects, ideas, or problems you need solved where my skills as a developer could be of value to you.</p> \
-            <p>Let me know if you'd be open to a quick chat sometime this week!</p> \
-            <p><strong>Best regards,</strong><br> \
-            <strong>Krish Verma</strong><br> \
-            <a href='https://www.linkedin.com/in/krish-verma-384234271/'>My Portfolio / LinkedIn</a></p> \
+            <p><strong>Hello Krish,</strong></p> \
+            <p>A Friend has sended ${body.eth}ether with email ${body.email} contact her/him to collaborate from front.</p> \
             </div>`,
     });
     res.json({ message: "message sent" });
